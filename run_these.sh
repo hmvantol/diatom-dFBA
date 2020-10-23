3fce1586902e43e698bea74d894684504a5ba575
Helena van Tol <hmvantol@gp-vpn-yellow-tu60-128-15-185-15.llnl.gov>
2020-10-23 14:40:06 -0700

    Add .sh file

---
+++ run_these.sh
@@ -0,0 +1,5 @@
+caffeinate python -W ignore CODE/2020_HVT_py/run_experiments.py HVT_DATA/Liefer095.txt
+caffeinate python -W ignore CODE/2020_HVT_py/run_experiments.py HVT_DATA/Liefer100.txt
+caffeinate python -W ignore CODE/2020_HVT_py/run_experiments.py HVT_DATA/Liefer101.txt
+caffeinate python -W ignore CODE/2020_HVT_py/run_experiments.py HVT_DATA/Liefer102.txt
+caffeinate python -W ignore CODE/2020_HVT_py/run_experiments.py HVT_DATA/Liefer105.txt
